c2a4bd9f6106c74f35138ead2b87da06f73fbe91
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-06 21:17:49 -0700

    Add typing

--- parse_configs/parse_market_config.py
+++ parse_configs/parse_market_config.py
@@ -7,16 +7,29 @@ class TaxBrackets(yaml.YAMLObject):
 	Due to using yaml_tag = "!TaxBrackets", the yaml library handles auto-
 	converting from a yaml file to an instance of this class. Therefore,
 	the __init__ method is not used.
+
+	Documentation of the instance variable types:
+		self.tax_brackets (List[Dict[str, float]]): A list of tax brackets,
+			where each bracket contains two keys, "upper_limit" and "tax_rate".
+			"tax_rate" is the marginal tax rate of that bracket. "upper_limit"
+			is the upper income limit of that bracket (beyond that limit, the
+			next tax bracket begins). This list is ordered from lowest tax
+			bracket to highest tax bracket. The highest tax bracket will have
+			an upper limit of positive infinity.
 	"""
 
-	yaml_tag = "!TaxBrackets"
+	yaml_tag: str = "!TaxBrackets"
 
 	# __init__ method not used due to yaml.YAMLObject
 
-	def get_tax(income):
+	def get_tax(income: float) -> float:
 		"""Calculates tax owed given income.
 
-		Income must be non-negative. Tax is always non-negative.
+		Args:
+			income: non-negative income
+
+		Returns:
+			tax: non-negative tax owed
 		"""
 		tax = 0
 		lower_limit = 0
@@ -38,14 +51,17 @@ class MarketConfig(yaml.YAMLObject):
 	Due to using yaml_tag = "!MarketConfig", the yaml library handles auto-
 	converting from a yaml file to an instance of this class. Therefore,
 	the __init__ method is not used.
+
+	Documentation of the instance variable types:
+		self.tax_brackets ('TaxBrackets'): A TaxBrackets object.
 	"""
 
-	yaml_tag = "!MarketConfig"
+	yaml_tag: str = "!MarketConfig"
 
 	# __init__ method not used due to yaml.YAMLObject
 
 	@staticmethod
-	def parse_market_config():
+	def parse_market_config() -> 'MarketConfig':
 		""" Load market config yaml file as an instance of this class """
 		# TODO replace this absolute path string literal
 		with open("/Users/rocky/Downloads/rent-buy-invest/configs/market-config.yaml") as f:
