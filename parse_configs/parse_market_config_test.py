b34725110e5910f3e7bb31b22198cc88223db747
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-08 22:48:54 -0700

    Add basic rent calculation

--- parse_configs/parse_market_config_test.py
+++ parse_configs/parse_market_config_test.py
@@ -8,6 +8,7 @@ class TestMarketConfig():
 
 	def test_get_tax(self) -> None:
 		# TODO don't use absolute path
+		# TODO use parse_market_config and pass in test config path
 		with open("/Users/rocky/Downloads/rent_buy_invest/parse_configs/test_config_files/2023-market-config.yaml") as f:
 			market_config = yaml.load(f)
 		# assert market_config.get_tax(0) == 0
