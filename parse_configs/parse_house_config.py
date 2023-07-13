4f777c1144c2364ff1e27de54b7bd420640db967
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-12 22:12:54 -0700

    Get upfront one time cost

--- parse_configs/parse_house_config.py
+++ parse_configs/parse_house_config.py
@@ -24,44 +24,44 @@ class HouseConfig(yaml.YAMLObject):
 		Raises:
 			AssertionError: If any house configs are invalid
 		"""
-		assert sale_price > 0
-		assert down_payment_fraction >= 0 and down_payment_fraction <= 1
-		assert mortgage_annual_interest_rate >= 0
-		assert mortgage_term_months > 0
-		assert pmi_fraction >= 0
-		assert mortgage_origination_points_fee >= 0
-		assert mortgage_processing_fee >= 0
-		assert mortgage_underwriting_fee >= 0
-		assert mortgage_discount_points_fee >= 0
-		assert house_appraisal_cost >= 0
-		assert credit_report_fee >= 0
-		assert transfer_tax_fraction >= 0
-		assert seller_burden_of_transfer_tax_fraction >= 0 and seller_burden_of_transfer_tax_fraction <= 1
-		assert recording_fee_fraction >= 0
-		assert monthly_property_tax_rate >= 0
-		assert realtor_commission_fraction >= 0
-		assert hoa_transfer_fee >= 0
-		assert seller_burden_of_hoa_transfer_fee >= 0 and seller_burden_of_hoa_transfer_fee <= 1
-		assert house_inspection_cost >= 0
-		assert pest_inspection_cost >= 0
-		assert escrow_fixed_fee >= 0
-		assert flood_certification_fee >= 0
-		assert title_search_fee >= 0
-		assert seller_burden_of_title_search_fee_fraction >= 0 and seller_burden_of_title_search_fee_fraction <= 1
-		assert attorney_fee >= 0
-		assert closing_protection_letter_fee >= 0
-		assert search_abstract_fee >= 0
-		assert survey_fee >= 0
-		assert notary_fee >= 0
-		assert deep_prep_fee >= 0
-		assert lenders_title_insurance_fraction >= 0
-		assert owners_title_insurance_fraction >= 0
-		assert endorsement_fees >= 0
-		assert monthly_homeowners_insurance_fraction >= 0
-		assert monthly_utilities >= 0
-		assert annual_maintenance_cost_fraction >= 0
-		assert monthly_hoa_fees >= 0
-		assert annual_management_cost_fraction >= 0
+		assert self.sale_price > 0
+		assert self.down_payment_fraction >= 0 and down_payment_fraction <= 1
+		assert self.mortgage_annual_interest_rate >= 0
+		assert self.mortgage_term_months > 0
+		assert self.pmi_fraction >= 0
+		assert self.mortgage_origination_points_fee >= 0
+		assert self.mortgage_processing_fee >= 0
+		assert self.mortgage_underwriting_fee >= 0
+		assert self.mortgage_discount_points_fee >= 0
+		assert self.house_appraisal_cost >= 0
+		assert self.credit_report_fee >= 0
+		assert self.transfer_tax_fraction >= 0
+		assert self.seller_burden_of_transfer_tax_fraction >= 0 and self.seller_burden_of_transfer_tax_fraction <= 1
+		assert self.recording_fee_fraction >= 0
+		assert self.monthly_property_tax_rate >= 0
+		assert self.realtor_commission_fraction >= 0
+		assert self.hoa_transfer_fee >= 0
+		assert self.seller_burden_of_hoa_transfer_fee >= 0 and self.seller_burden_of_hoa_transfer_fee <= 1
+		assert self.house_inspection_cost >= 0
+		assert self.pest_inspection_cost >= 0
+		assert self.escrow_fixed_fee >= 0
+		assert self.flood_certification_fee >= 0
+		assert self.title_search_fee >= 0
+		assert self.seller_burden_of_title_search_fee_fraction >= 0 and self.seller_burden_of_title_search_fee_fraction <= 1
+		assert self.attorney_fee >= 0
+		assert self.closing_protection_letter_fee >= 0
+		assert self.search_abstract_fee >= 0
+		assert self.survey_fee >= 0
+		assert self.notary_fee >= 0
+		assert self.deep_prep_fee >= 0
+		assert self.lenders_title_insurance_fraction >= 0
+		assert self.owners_title_insurance_fraction >= 0
+		assert self.endorsement_fees >= 0
+		assert self.monthly_homeowners_insurance_fraction >= 0
+		assert self.monthly_utilities >= 0
+		assert self.annual_maintenance_cost_fraction >= 0
+		assert self.monthly_hoa_fees >= 0
+		assert self.annual_management_cost_fraction >= 0
 
 	def get_down_payment(self):
 		return self.down_payment_fraction * self.sale_price
@@ -78,6 +78,32 @@ class HouseConfig(yaml.YAMLObject):
 		L = self.get_loan_amount()
 		return L * (1 - r) / (r - r**(n+1))
 
+	def get_upfront_one_time_cost(self):
+		return self.mortgage_origination_points_fee
+			+ self.mortgage_processing_fee
+			+ self.mortgage_underwriting_fee
+			+ self.mortgage_discount_points_fee
+			+ self.house_appraisal_cost
+			+ self.credit_report_fe
+			+ (1 - self.seller_burden_of_transfer_tax_fraction) * self.transfer_tax_fraction * self.sale_price
+			+ (self.recording_fee_fraction * self.sale_price)
+			+ (self.realtor_commission_fraction * self.sale_price)
+			+ (1 - self.seller_burden_of_hoa_transfer_fee) * self.hoa_transfer_fee
+			+ self.house_inspection_cost
+			+ self.pest_inspection_cost
+			+ self.escrow_fixed_fee
+			+ self.flood_certification_fee
+			+ (1 - self.seller_burden_of_title_search_fee_fraction) * self.title_search_fee
+			+ self.attorney_fee
+			+ self.closing_protection_letter_fee
+			+ self.search_abstract_fee
+			+ self.survey_fee
+			+ self.notary_fee
+			+ self.deep_prep_fee
+			+ self.lenders_title_insurance_fraction * self.get_loan_amount()
+			+ self.owners_title_insurance_fraction * self.get_loan_amount()
+			+ self.endorsement_fees
+	
 	# TODO test this and all parse methods
 	@staticmethod
 	def parse_house_config() -> 'HouseConfig':
