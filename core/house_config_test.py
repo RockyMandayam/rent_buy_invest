4388fefe44d1491136f67d601e191a2005346281
RockyMandayam <rocky.mandayam@gmail.com>
2023-07-21 03:06:04 -0700

    Add house config tests (#43)

--- core/house_config_test.py
+++ core/house_config_test.py
@@ -5,8 +5,47 @@ import yaml
 from ..utils import io_utils
 from .house_config import HouseConfig
 
+filename = (
+    "/Users/rocky/Downloads/rent_buy_invest/core/test_resources/test-house-config.yaml"
+)
+house_config = HouseConfig.parse(filename)
+
 
 class TestHouseConfig:
-    def test_parse(self) -> None:
-        filename = "/Users/rocky/Downloads/rent_buy_invest/core/test_resources/test-house-config.yaml"
-        HouseConfig.parse(filename)
+    def test_get_monthly_mortgage_payment(self) -> None:
+        # Sale price is $500,000. Down payment is 20%. So loan amount is $400,000
+        # Mortgage term is 360 months
+        # Annual interest rate is 0.06
+        actual = house_config.get_monthly_mortgage_payment()
+        expected = 2398.20
+        assert actual == expected
+
+    def test_get_upfront_one_time_cost(self) -> None:
+        actual = house_config.get_upfront_one_time_cost()
+        expected = (
+            0.015 * 400000
+            + 300
+            + 500
+            + 0.005 * 400000
+            + 500
+            + 50
+            + (1 - 0.9) * 0.0011 * 500000
+            + (0.03 * 500000)
+            + (0.025 * 500000)
+            + (1 - 1.0) * 300
+            + 500
+            + 500
+            + 500
+            + 20
+            + (1 - 1) * 100
+            + 800
+            + 35
+            + 300
+            + 500
+            + 100
+            + 50
+            + 0.02 * 400000
+            + 0.01 * 400000
+            + 150
+        )
+        assert actual == expected
