8693062f986a08a22bd76e5799b41224cd548ba2
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-21 15:07:14 -0700

    Format

--- core/general_config_test.py
+++ core/general_config_test.py
@@ -8,5 +8,7 @@ from .general_config import GeneralConfig
 
 class TestGeneralConfig:
     def test_parse(self) -> None:
-        filename = path_utils.get_abs_path("rent_buy_invest/core/test_resources/test-general-config.yaml")
+        filename = path_utils.get_abs_path(
+            "rent_buy_invest/core/test_resources/test-general-config.yaml"
+        )
         GeneralConfig.parse(filename)
--- utils/io_utils_test.py
+++ utils/io_utils_test.py
@@ -6,7 +6,9 @@ from . import io_utils, path_utils
 
 
 def test_load_yaml() -> None:
-    filename = path_utils.get_abs_path("rent_buy_invest/utils/test_resources/simple-yaml.yaml")
+    filename = path_utils.get_abs_path(
+        "rent_buy_invest/utils/test_resources/simple-yaml.yaml"
+    )
     with open(filename) as f:
         actual: Dict[str, Any] = yaml.load(f, Loader=yaml.SafeLoader)
     expected = {
