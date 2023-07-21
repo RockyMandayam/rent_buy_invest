441b733bfd0bb3440cf2c168b8ad9032f172cebb
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-21 14:31:40 -0700

    Update rent config tests

--- core/rent_config_test.py
+++ core/rent_config_test.py
@@ -3,21 +3,17 @@ import yaml
 from ..utils import io_utils
 from .rent_config import RentConfig
 
+# TODO don't use absolute path
+filename = (
+    "/Users/rocky/Downloads/rent_buy_invest/core/test_resources/test-rent-config.yaml"
+)
+rent_config = RentConfig.parse(filename)
+
 
 class TestRentConfig:
     # TODO test edge cases
 
-    def test_parse(self) -> None:
-        filename = "/Users/rocky/Downloads/rent_buy_invest/core/test_resources/test-rent-config.yaml"
-        RentConfig.parse(filename)
-
     def test_get_monthly_costs_of_renting(self) -> None:
-        # TODO don't use absolute path
-        # TODO use rent_config and pass in test config path
-        rent_config = io_utils.load_yaml(
-            "/Users/rocky/Downloads/rent_buy_invest/core/test_resources/test-rent-config.yaml"
-        )
-        rent_config = RentConfig(**rent_config)
         actual = rent_config.get_monthly_costs_of_renting(25)
         expected = (
             [2420.0] * 12
