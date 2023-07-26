d238545dd4c738a93935433aa3b2b71cb4c66b6c
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-07-25 18:32:03 -0700

    Minor updates

--- core/house_calculator.py
+++ core/house_calculator.py
@@ -6,6 +6,7 @@ from rent_buy_invest.core.rent_config import RentConfig
 
 
 class HouseCalculator:
+    """ Calculates projection of financial state when owning a house. """
     def __init__(
         self,
         house_config: HouseConfig,
@@ -13,12 +14,17 @@ class HouseCalculator:
         num_months: int,
         initial_state: InitialState,
     ):
-        self.house_config = house_config
-        self.rent_config = rent_config
-        self.num_months = num_months
-        self.initial_state = initial_state
+        self.house_config: HouseConfig = house_config
+        self.rent_config: RentConfig = rent_config
+        self.num_months: int = num_months
+        self.initial_state: InitialState = initial_state
 
     def calculate(self) -> Tuple[Tuple[str, List[float]]]:
+        """Calculates projection of financial state when owning a house.
+
+        Returns:
+            # TODO document this. Mention how it returns a nested tuple instead of a dict due to order
+        """
         house_values = self.house_config.get_monthly_house_values(self.num_months)
         house_monthly_costs_related_to_house_value = (
             self.house_config.get_house_value_related_monthly_costs(self.num_months)
@@ -46,8 +52,8 @@ class HouseCalculator:
             paid_toward_equity.append(round(toward_equity, 2))
             equities.append(round(house_values[month] - mortgage_amount, 2))
             pmis.append(self.house_config.pmi_fraction * mortgage_amount)
-            # projection.append(month_row)
             mortgage_amount -= toward_equity
+            assert mortgage_annual_interest_rate >= 0, "Mortgage amount cannot be negative."
         return (
             (
                 "House value related monthly cost",
