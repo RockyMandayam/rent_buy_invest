135e95ad28b6aeb92e37566faf0dce28ff38f97a
Rocky Mandayam <rocky.mandayam@gmail.com>
2023-08-12 12:41:20 -0700

    Add experiment config tests

--- core/experiment_config_test.py
+++ core/experiment_config_test.py
@@ -1,4 +1,52 @@
+import jsonschema
+import pytest
+
 from rent_buy_invest.core.experiment_config import ExperimentConfig
+from rent_buy_invest.core.utils_for_testing import (
+    check_filepath_field,
+    check_float_field,
+)
+from rent_buy_invest.utils import io_utils
 
 TEST_CONFIG_PATH = "rent_buy_invest/core/test_resources/test-experiment-config.yaml"
 EXPERIMENT_CONFIG = ExperimentConfig.parse(TEST_CONFIG_PATH)
+
+
+class TestExperimentConfig:
+    def test_inputs_with_invalid_schema(self) -> None:
+        attributes = [
+            "num_months",
+            "market_config_path",
+            "rent_config_path",
+            "house_config_path",
+        ]
+        for attribute in attributes:
+            test_config_filename = f"rent_buy_invest/core/test_resources/test-experiment-config_null_{attribute}.yaml"
+            with pytest.raises(jsonschema.ValidationError):
+                ExperimentConfig.parse(test_config_filename)
+
+    def test_invalid_inputs(self) -> None:
+        config_kwargs = io_utils.read_yaml(TEST_CONFIG_PATH)
+
+        check_float_field(
+            ExperimentConfig,
+            config_kwargs,
+            ["num_months"],
+            allow_negative=False,
+            allow_zero=False,
+        )
+        check_filepath_field(
+            ExperimentConfig,
+            config_kwargs,
+            ["market_config_path"],
+        )
+        check_filepath_field(
+            ExperimentConfig,
+            config_kwargs,
+            ["rent_config_path"],
+        )
+        check_filepath_field(
+            ExperimentConfig,
+            config_kwargs,
+            ["house_config_path"],
+        )
--- core/utils_for_testing.py
+++ core/utils_for_testing.py
@@ -1,15 +1,19 @@
 import copy
-from typing import List, Type
+from typing import Any, Dict, List, Type
 
 import pytest
-
-from rent_buy_invest.core.config import Config
 
 # TODO test this file
 # TODO document this file
 
 
-def check_field(clz, config_kwargs, field_keys, invalid_values):
+def _check_field(
+    clz: Type,
+    config_kwargs: Dict[str, Any],
+    field_keys: List,
+    invalid_values: List,
+    exception_to_raise: Exception,
+):
     assert field_keys, "field_keys must be a non-empty list"
     config_kwargs = copy.deepcopy(config_kwargs)
     field = config_kwargs
@@ -17,13 +21,13 @@ def check_field(clz, config_kwargs, field_keys, invalid_values):
         field = field[field_keys[i]]
     for val in invalid_values:
         field[field_keys[-1]] = val
-        with pytest.raises(AssertionError):
+        with pytest.raises(exception_to_raise):
             clz(**config_kwargs)
 
 
 def check_float_field(
     clz: Type,
-    config_kwargs: Config,
+    config_kwargs: Dict[str, Any],
     field_keys: List,
     allow_negative: bool = True,
     allow_zero: bool = True,
@@ -37,4 +41,19 @@ def check_float_field(
         invalid_values.append(0)
     if not allow_greater_than_one:
         invalid_values.append(1.1)
-    check_field(clz, config_kwargs, field_keys, invalid_values)
+    _check_field(clz, config_kwargs, field_keys, invalid_values, AssertionError)
+
+
+def check_filepath_field(
+    clz: Type,
+    config_kwargs: Dict[str, Any],
+    field_keys: List,
+) -> None:
+    _check_field(clz, config_kwargs, field_keys, ["nonexistent_filepath"], ValueError)
+    _check_field(
+        clz,
+        config_kwargs,
+        field_keys,
+        ["rent_buy_invest/nonexistent_filepath"],
+        FileNotFoundError,
+    )
