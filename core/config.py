e02899c38f0108ff03527c06dcc0b674be5e28e1
Rocky Mandayam <rockymandayam@Rockys-MacBook-Pro-3.local>
2024-12-30 13:15:46 -0800

    Add some util methods for validation and use them in rent config assertions instead

--- core/config.py
+++ core/config.py
@@ -34,3 +34,21 @@ class Config(ABC):
         config_kwargs = io_utils.read_yaml(project_path)
         jsonschema.validate(instance=config_kwargs, schema=config_schema)
         return cls(**config_kwargs)
+
+    def _validate_max_value(self, attr_name: str, max_value: float) -> None:
+        attr_val = getattr(self, attr_name)
+        assert (
+            attr_val <= max_value
+        ), f"Please set '{attr_name}' to something reasonable (at most {max_value})"
+
+    def _validate_max_value_as_fraction(
+        self,
+        attr_name: str,
+        reference_attr_name: str,
+        max_value_as_fraction_of_reference_value: float,
+    ) -> None:
+        attr_val = getattr(self, attr_name)
+        reference_attr_val = getattr(self, reference_attr_name)
+        assert (
+            attr_val <= max_value_as_fraction_of_reference_value * reference_attr_val
+        ), f"Please set '{attr_name}' to something reasonable (at most {max_value_as_fraction_of_reference_value} times '{reference_attr_name}')"
--- core/rent_config.py
+++ core/rent_config.py
@@ -72,27 +72,30 @@ class RentConfig(Config):
             self.unrecoverable_fraction_of_security_deposit >= 0
             and self.unrecoverable_fraction_of_security_deposit <= 1
         ), "Unrecoverable fraction of security deposit must be between 0 and 1 inclusive."
-        assert (
-            self.monthly_utilities
-            <= RentConfig.MAX_MONTHLY_UTILITIES_AS_FRACTION_OF_RENT * self.monthly_rent
-        ), f"Please set monthly utilities to something reasonable (at most {RentConfig.MAX_MONTHLY_UTILITIES_AS_FRACTION_OF_RENT} of monthly rent)"
-        assert (
-            self.monthly_renters_insurance
-            <= RentConfig.MAX_MONTHLY_RENTERS_INSURANCE_AS_FRACTION_OF_RENT
-            * self.monthly_rent
-        ), f"Please set monthly renters insurance to something reasonable (at most {RentConfig.MAX_MONTHLY_RENTERS_INSURANCE_AS_FRACTION_OF_RENT} of monthly rent)"
-        assert (
-            self.monthly_parking_fee
-            <= RentConfig.MAX_MONTHLY_PARKING_FEE_AS_FRACTION_OF_RENT
-            * self.monthly_rent
-        ), f"Please set the monthly parking fee to something reasonable (at most {RentConfig.MAX_MONTHLY_PARKING_FEE_AS_FRACTION_OF_RENT} of monthly rent)"
-        assert (
-            self.annual_rent_inflation_rate <= RentConfig.MAX_ANNUAL_RENT_INFLATION_RATE
-        ), f"Please set the annual rent inflation rent to something reasonable (at most {RentConfig.MAX_ANNUAL_RENT_INFLATION_RATE})"
-        assert (
-            self.security_deposit
-            <= RentConfig.MAX_SECURITY_DEPOSIT_AS_FRACTION_OF_RENT * self.monthly_rent
-        ), f"Please set the security deposit to something reasonable (at most {RentConfig.MAX_MONTHLY_PARKING_FEE_AS_FRACTION_OF_RENT} times monthly rent)"
+        self._validate_max_value_as_fraction(
+            "monthly_utilities",
+            "monthly_rent",
+            RentConfig.MAX_MONTHLY_UTILITIES_AS_FRACTION_OF_RENT,
+        )
+
+        self._validate_max_value_as_fraction(
+            "monthly_renters_insurance",
+            "monthly_rent",
+            RentConfig.MAX_MONTHLY_RENTERS_INSURANCE_AS_FRACTION_OF_RENT,
+        )
+        self._validate_max_value_as_fraction(
+            "monthly_parking_fee",
+            "monthly_rent",
+            RentConfig.MAX_MONTHLY_PARKING_FEE_AS_FRACTION_OF_RENT,
+        )
+        self._validate_max_value(
+            "annual_rent_inflation_rate", RentConfig.MAX_ANNUAL_RENT_INFLATION_RATE
+        )
+        self._validate_max_value_as_fraction(
+            "security_deposit",
+            "monthly_rent",
+            RentConfig.MAX_SECURITY_DEPOSIT_AS_FRACTION_OF_RENT,
+        )
 
     def get_upfront_one_time_cost(self) -> float:
         return self.security_deposit * self.unrecoverable_fraction_of_security_deposit
