27c1aaf75184fbd6e134886b4bb48e60b541dbbb
RockyMandayam <rocky.mandayam@gmail.com>
2025-01-03 00:04:46 -0800

    move utils_for_testing to configs (#145)
    Co-authored-by: Rocky Mandayam <rockymandayam@Rockys-MacBook-Pro-3.local>

--- configs/buy_config_test.py
+++ configs/buy_config_test.py
@@ -2,7 +2,7 @@ import jsonschema
 import pytest
 
 from rent_buy_invest.configs.buy_config import BuyConfig
-from rent_buy_invest.core.utils_for_testing import check_float_field
+from rent_buy_invest.configs.utils_for_testing import check_float_field
 from rent_buy_invest.utils import io_utils
 
 TEST_CONFIG_PATH = "rent_buy_invest/core/test_resources/test-buy-config.yaml"
