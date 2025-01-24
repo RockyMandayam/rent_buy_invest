1b2db6a7ca269973ad6dc61a041800e5160e0174
Rocky Mandayam <rocky.mandayam@gmail.com>
2025-01-23 17:56:19 -0800

    rm old version of test_get_upfront_one_time_cost

--- configs/buy_config_test.py
+++ configs/buy_config_test.py
@@ -560,33 +560,3 @@ class TestBuyConfig(TestConfig):
             2,
         )
         assert actual == expected
-
-    def test_get_upfront_one_time_cost(self) -> None:
-        actual = TestBuyConfig.BUY_CONFIG.get_upfront_one_time_cost()
-        expected = (
-            0.015 * 400000
-            + 300
-            + 500
-            + 0.005 * 400000
-            + 500
-            + 50
-            + (1 - 0.9) * 0.0011 * 500000
-            + (0.03 * 500000)
-            + (0.025 * 500000)
-            + (1 - 1.0) * 300
-            + 500
-            + 500
-            + 500
-            + 20
-            + (1 - 1) * 100
-            + 800
-            + 35
-            + 300
-            + 500
-            + 100
-            + 50
-            + 0.02 * 400000
-            + 0.01 * 400000
-            + 150
-        )
-        assert actual == expected
